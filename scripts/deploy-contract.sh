a991388ea7ad453cdf3db4aef6e4ad97ca941b86
Benjamin Moore <ben.wilson.moore@gmail.com>
2023-12-29 19:06:00 +0000

    Automatically determine transaction fee to deploy contract

--- scripts/deploy-contract.sh
+++ scripts/deploy-contract.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-cardano-cli transaction calculate-min-required-utxo \
- --testnet-magic $CARDANO_NODE_MAGIC \
- --change-address $(cat my_address.addr) \
- --tx-in $1 \
- --tx-out "$(cat $2.addr) $3 lovelace" \
+output=$(cardano-cli transaction calculate-min-required-utxo --babbage-era \
+ --protocol-params-file params.json \
  --tx-out-reference-script-file $2.plutus \
- --tx-out-datum-hash-file unit.json \
- --out-file raw.tx
+ --tx-out "$(cat $2.addr) + 0")
+
+datacost=$(cut -d' ' -f2 <<< "$output")
 
 cardano-cli transaction build \
  --testnet-magic $CARDANO_NODE_MAGIC \
  --change-address $(cat my_address.addr) \
  --tx-in $1 \
- --tx-out "$(cat $2.addr) 6335700 lovelace" \
+ --tx-out "$(cat $2.addr) $datacost lovelace" \
  --tx-out-reference-script-file $2.plutus \
  --tx-out-datum-hash-file unit.json \
  --out-file raw.tx
