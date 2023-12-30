29ea228902d076557c8d4d347d33f1044bc4572f
Benjamin Moore <ben.wilson.moore@gmail.com>
2023-12-30 22:56:59 +0000

    Update parameters and transaction generation

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -4,6 +4,12 @@ tokenname=$(echo -n "$4" | xxd -ps | tr -d '\n')
 tokenname="000643b0$tokenname"
 echo "Token name encoded: $tokenname"
 
+output=$(cardano-cli transaction calculate-min-required-utxo \
+ --protocol-params-file params.json \
+ --tx-out $(cat $3.addr)+0+"1 $(cat $3.pol).$tokenname")
+
+datacost=$(cut -d' ' -f2 <<< "$output")
+
 cardano-cli transaction build --testnet-magic ${CARDANO_NODE_MAGIC} \
  --tx-in $1 \
  --tx-in-collateral $1 \
@@ -12,7 +18,8 @@ cardano-cli transaction build --testnet-magic ${CARDANO_NODE_MAGIC} \
  --mint-reference-tx-in-redeemer-file ./mint-redeemer.json \
  --policy-id $(cat $3.pol) \
  --mint "1 $(cat $3.pol).$tokenname" \
- --tx-out $(cat $3.addr)+0+"1 $(cat $3.pol).$tokenname" \
+ --tx-out $(cat $3.addr)+$datacost+"1 $(cat $3.pol).$tokenname" \
+ --required-signer-hash $(cardano-cli address key-hash --payment-verification-key-file my_address.vkey) \
  --change-address $(cat $3.addr) \
  --out-file mint.tx
 
@@ -22,6 +29,6 @@ cardano-cli transaction sign \
  --testnet-magic $CARDANO_NODE_MAGIC \
  --out-file mint-signed.tx
 
-#cardano-cli transaction submit \
-# --testnet-magic $CARDANO_NODE_MAGIC \
-# --tx-file ./mint-signed.tx
+cardano-cli transaction submit \
+ --testnet-magic $CARDANO_NODE_MAGIC \
+ --tx-file ./mint-signed.tx
--- scripts/parameterize-validator.sh
+++ scripts/parameterize-validator.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cp ../plutus.json .
-node param-application.js "oneshot.handle_subscription" addr_test1vzg8xpjql48xz47ggec2xv0664aqp4z0n8xdltlj6x3pwxsajlk4w
+
+node param-application.js "oneshot.handle_subscription" $(cardano-cli address key-hash --payment-verification-key-file my_address.vkey)
