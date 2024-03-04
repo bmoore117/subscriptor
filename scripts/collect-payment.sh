070ab6ed6d31a10e5d24f6ab2c65551f8998eb77
Benjamin Moore <ben.wilson.moore@gmail.com>
2024-03-04 02:33:32 +0000

    Start on spend script

---
+++ scripts/collect-payment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# need: redeemer, multiple inputs including the right datum, and a reference input of the platform
+# also need: to generate proper output datum
+
+json_data=$(cat collect-payment.json)
