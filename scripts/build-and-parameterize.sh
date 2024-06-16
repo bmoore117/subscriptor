142a9694f9c6adf47776036fbc136b5b28885ea7
Benjamin Moore <ben.wilson.moore@gmail.com>
2024-06-16 17:24:29 +0000

    Build and parameterize in a single step

--- scripts/build-and-parameterize.sh
+++ scripts/build-and-parameterize.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cp ../plutus.json .
-
+cd ..
+aiken build -t verbose
+mv plutus.json scripts
+cd scripts
 node param-application.js "subscriptor.handle_subscription" $(cardano-cli address key-hash --payment-verification-key-file intermediate/user.vkey) $(cat intermediate/platform-policy.id)
