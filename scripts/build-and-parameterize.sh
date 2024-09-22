80ceffc5c002046cab0be0024c0ba909a3006258
Benjamin Moore <ben.wilson.moore@gmail.com>
2024-09-22 01:46:15 +0000

    Fix generation of policy id

--- scripts/build-and-parameterize.sh
+++ scripts/build-and-parameterize.sh
@@ -6,3 +6,4 @@ mv plutus.json scripts
 cd scripts
 node param-application.js "subscriptor.subscriptor.spend" $(cardano-cli address key-hash --payment-verification-key-file intermediate/user.vkey) $(cat intermediate/platform-policy.id)
 cardano-cli conway address build --payment-script-file subscriptor.subscriptor.spend.plutus --out-file subscriptor.subscriptor.spend.addr
+cat subscriptor.subscriptor.spend.addr | bin/cardano-address address inspect | jq -r '.spending_shared_hash' > subscriptor.subscriptor.spend.pol
